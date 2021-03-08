abe432e589349403d514af5e33c924502ea2f25c
Tiago Lira <tiago.lira.p@gmail.com>
2021-03-08 12:24:59 +0000

    Updated framework

---
+++ InTheGame/InTheGameBC.xcframework/ios-x86_64-simulator/InTheGameBC.framework/Headers/InTheGameBC.h
@@ -0,0 +1,19 @@
+//
+//  InTheGameBC.h
+//  InTheGameBC
+//
+//  Created by Tiago Lira on 09/12/2019.
+//  Copyright © 2019 In The Game. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InTheGameBC.
+FOUNDATION_EXPORT double InTheGameBCVersionNumber;
+
+//! Project version string for InTheGameBC.
+FOUNDATION_EXPORT const unsigned char InTheGameBCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InTheGameBC/PublicHeader.h>
+
+
